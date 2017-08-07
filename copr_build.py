1e23474d1bd2a67e1173b42f9785d3a40baeb03a
Till Hofmann <thofmann@fedoraproject.org>
2017-08-07 14:44:08 +0200

    copr_build: switch from raw RESTful API to python copr-cli
    There is an existing wrapper for python, use the wrapper instead of
    sending requests directly to the RESTful API.

--- copr_build.py
+++ copr_build.py
@@ -12,22 +12,21 @@ the specified COPR and if not, will submit a new build to the COPR.
 """
 
 import argparse
+import copr
 import json
 import re
-import requests
 import subprocess
 
 """ Base URL of the COPR, will be appended with the API URL suffix. """
 copr_url = 'https://copr.fedorainfracloud.org'
 
-def build_spec(project_id, chroot, spec, auth):
+def build_spec(project_id, chroot, spec):
     """ Build a package in COPR from a SPEC file.
 
     Args:
         project_id: The COPR project where the package should be built.
         chroot: The chroot to use for the build, e.g., fedora-26-x86_64
         spec: The path to the SPEC file of the package.
-        auth: A tuple (user, password) used with BasicAuth for the COPR API.
     """
     print('Building {} for chroot {}'.format(spec, chroot))
     res = subprocess.run(['rpmbuild', '-bs', spec],
@@ -37,31 +36,26 @@ def build_spec(project_id, chroot, spec, auth):
     match = re.match('Wrote: (\S+)', res.stdout)
     assert match, 'Unexpected output from rpmbuild: "%s"'.format(res.stdout)
     srpm = match.group(1)
-    build_srpm(project_id, chroot, srpm, auth)
+    build_srpm(project_id, chroot, srpm)
 
-def build_srpm(project_id, chroot, srpm, auth):
+def build_srpm(project_id, chroot, srpm):
     """ Build a package in COPR from a SRPM.
 
     Args:
         project_id: The COPR project where the package should be built.
         chroot: The chroot to use for the build, e.g., fedora-26-x86_64
         srpm: The path to the SRPM file of the package.
-        auth: A tuple (user, password) used with BasicAuth for the COPR API.
     """
     print('Building {} for project {} with chroot {}'.format(
         srpm, project_id, chroot))
-    metadata = {
-        'project_id': project_id,
-        'chroots': [chroot],
-        'enable_net': False
-    }
-    r = requests.post(url=copr_url + '/api_2/builds',
-                      files={'file': open(srpm, 'rb')},
-                      json=json.dumps(metadata),
-                      auth=auth)
-    print(r)
-    assert r.status_code == 200, \
-            'Request failed, returned {}'.format(r.status_code)
+    copr_client = copr.create_client2_from_file_config()
+    build = copr_client.builds.create_from_file(project_id=project_id,
+                                                file_path=srpm)
+    while not build.is_finished():
+        build = build.get_self()
+    assert build.state == 'succeeded', \
+            'Build failed, state is {}.'.format(build.state)
+    print('Building {} was successful.'.format(srpm))
 
 def pkg_is_built(copr_project_id, chroot, pkg_name):
     """ Check if the given package is already built in the COPR.
@@ -74,18 +68,16 @@ def pkg_is_built(copr_project_id, chroot, pkg_name):
     Returns:
         True iff the package was already built in the given project and chroot.
     """
-    builds = json.loads(requests.get(copr_url + '/api_2/builds',
-                          {'project_id': copr_project_id}).text)['builds']
+    copr_client = copr.create_client2_from_file_config()
+    builds = copr_client.builds.get_list(copr_project_id)
     for build in builds:
-        if build['build']['package_name'] == pkg_name:
-            build_info = json.loads(requests.get(
-                copr_url + build['_links']['build_tasks']['href']).text)
-            # We can always use the first item in the list because we
-            # query for the build id and therefore there is only one.
-            build_task = build_info['build_tasks'][0]['build_task']
-            if build_task['state'] == 'succeeded' and \
-               build_task['chroot_name'] == chroot:
-                return True
+        if build.package_name == pkg_name:
+            build_tasks = build.get_build_tasks()
+            for build_task in build_tasks:
+                # TODO: add version check
+                if build_task.state == 'succeeded' and \
+                   build_task.chroot_name == chroot:
+                    return True
     return False
 
 def main():
@@ -93,23 +85,17 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--chroot', action='append',
                         help='The chroot(s) to use for the packages')
-    parser.add_argument('--project_id', default='14923',
+    parser.add_argument('--project-id', type=int, default=14923,
                         help='The COPR project ID to build the packages in')
     parser.add_argument('--spec-dir', default='./specs/',
                         help='The directory where to look for SPEC files')
-    parser.add_argument('-u', '--user',
-                        help='The COPR API user which is used to send requests')
-    parser.add_argument('-p', '--password',
-                        help='The password to use to send requests to COPR')
     parser.add_argument('pkg_name', nargs='+')
     args = parser.parse_args()
     for chroot in args.chroot:
         for pkg in args.pkg_name:
             if not pkg_is_built(args.project_id, chroot, pkg):
                 spec = args.spec_dir + pkg + '.spec'
-                build_spec(project_id=args.project_id, chroot=chroot, spec=spec,
-                           auth=(args.user, args.password))
-
+                build_spec(project_id=args.project_id, chroot=chroot, spec=spec)
 
 if __name__ == '__main__':
     main()
